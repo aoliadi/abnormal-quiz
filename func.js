376f7016770f253b0fba74c18814e26d5d390d52
aoliadi <aoliadi1998@gmail.com>
2020-05-09 19:04:03 +0100

    First commit

---
+++ func.js
@@ -0,0 +1,44 @@
+//FOR QUESTION1
+let showQuestion1 = document.querySelector(".next1")
+    showQuestion1.addEventListener("click", displayQuestion1)
+let question1 = document.querySelector(".question1")
+function displayQuestion1() {
+    question1.classList.add("hide");
+    
+}
+
+//FOR QUESTION2
+let showQuestion2 = document.querySelector(".next1")
+    showQuestion2.addEventListener("click", displayQuestion2)
+let question2 = document.querySelector(".question2")
+
+function displayQuestion2() {
+    question2.classList.add("hide");
+}
+
+//FOR QUESTION3
+let showQuestion3 = document.querySelector(".next3")
+    showQuestion3.addEventListener("click", displayQuestion3)
+let question3 = document.querySelector(".question3")
+
+function displayQuestion3() {
+    question3.classList.add("hide");
+}
+
+//FOR QUESTION4
+let showQuestion4 = document.querySelector(".next4")
+    showQuestion4.addEventListener("click", displayQuestion4)
+let question4 = document.querySelector(".question4")
+
+function displayQuestion4() {
+    question4.classList.add("hide");
+}
+
+//FOR QUESTION5
+let showQuestion5 = document.querySelector(".next5")
+    showQuestion5.addEventListener("click", displayQuestion5)
+let question5 = document.querySelector(".question5")
+
+function displayQuestion5() {
+    question5.classList.add("hide");
+}
